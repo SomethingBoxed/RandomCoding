a58af01161b525768ef5833b95e9a5bd9c8e1e0f
SomethingBoxed <163446862+SomethingBoxed@users.noreply.github.com>
2025-09-23 08:46:46 +0800

    text file generator history

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using textfile_generator.Properties;
+
+namespace textfile_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fcontent = file_content.Text;
+            string fname =file_name.Text;
+
+            File.WriteAllText(fname + ".txt", fcontent);  // Create a file and write the content of writeText to it
+
+        }
+
+        private void file_content_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fish_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void normal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
